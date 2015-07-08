690006350a1a191d6601d06f1607e158375ab9b8
hpetrow <heather.petrow@gmail.com>
2015-07-08 13:50:09 -0400

    done

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,31 @@
 # Enter your procedural solution here!
+
+def largest_prime_factor(n)
+  primes = []
+  prime_factors(n, primes)
+  largest = primes.max
+end
+
+def prime_factors(n, primes)
+  factors = tree_factors(n)
+  if (factors)
+    factors.each{ |factor|
+      prime_factors(factor, primes)
+    }
+  else
+    primes << n
+  end
+end
+
+def tree_factors(n)
+  factors = nil
+  i = Math.sqrt(n).floor
+
+  while i >= 2 && !factors
+    if n%i == 0
+      factors = [i, n/i]
+    end
+    i -= 1
+  end
+  factors
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,39 @@
 # Enter your object-oriented solution here!
+
+class LargestPrimeFactor
+  attr_reader :number
+
+  def initialize(n)
+    @number = self.largest_prime_factor(n)
+  end
+
+  def largest_prime_factor(n)
+    primes = []
+    prime_factors(n, primes)
+    primes.max
+  end
+
+  def prime_factors(n, primes)
+    factors = tree_factors(n)
+    if (factors)
+      factors.each{ |factor|
+        prime_factors(factor, primes)
+      }
+    else
+      primes << n
+    end
+  end
+
+  def tree_factors(n)
+    factors = nil
+    i = Math.sqrt(n).floor
+
+    while i >= 2 && !factors
+      if n%i == 0
+        factors = [i, n/i]
+      end
+      i -= 1
+    end
+    factors
+  end
+end
